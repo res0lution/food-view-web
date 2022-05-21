c2992051220d9562f19a16743d0a3614dfde0763
Res0ution <maestro@hotger.com>
2022-05-21 18:21:58 +0300

    add login page

--- food_view/core/urls.py
+++ food_view/core/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('account/sign_in', auth_views.LoginView.as_view(template_name='sign_in.html'), name='sign_in'),
+    path('account/sign_out', auth_views.LogoutView.as_view(next_page='/'), name='sign_out'),
+    path('restaurant', views.home, name=''),
 ]
