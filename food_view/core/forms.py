c6fac11abe4b6d6193e284e92e08d23e67769d5d
Res0ution <maestro@hotger.com>
2022-06-04 14:31:34 +0300

    register restaurant model

--- food_view/core/forms.py
+++ food_view/core/forms.py
@@ -2,6 +2,9 @@ from django.contrib.auth.models import User
 from django import forms
 
 class UserForm(forms.ModelForm):
+    email = forms.CharField(max_length=100, required=True)
+    password = forms.CharField(widget=forms.PasswordInput())
+
     class Meta:
         model = User
         fields = ("username", "password", "first_name", "last_name", "email")
--- food_view/core/views.py
+++ food_view/core/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 from core.forms import UserForm
 from restaurant.forms import RestaurantForm
@@ -13,6 +15,20 @@ def home(request):
 def sign_up(request):
     user_form = UserForm()
     restaurant_form = RestaurantForm()
+
+    if request.method == 'POST':
+        user_form = UserForm(request.POST)
+        restaurant_form = RestaurantForm(request.POST, request.FILES)
+
+        if user_form.is_valid() and restaurant_form.is_valid():
+            new_user = User.objects.create(**user_form.cleaned_data)
+            new_restaurant = restaurant_form.save(commit=False)
+            new_restaurant.user = new_user
+            new_restaurant.save()
+
+            login(request, new_user, backend='django.contrib.auth.backends.ModelBackend')
+
+            return redirect(request, 'home.html', {})
 
     return render(request, 'sign_up.html', {
         "user_form": user_form,
