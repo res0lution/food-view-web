9123318205894479b2001f5c8f80ee6b4d01335f
Res0ution <maestro@hotger.com>
2022-07-02 15:12:13 +0300

    create edit meal design page

--- food_view/restaurant/urls.py
+++ food_view/restaurant/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('meal', views.meal, name='meal'),
     path('meal/add', views.add_meal, name='add_meal'),
+    path('meal/edit/<int:meal_id>', views.edit_meal, name='edit_meal')
 ]
--- food_view/restaurant/views.py
+++ food_view/restaurant/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+
 from .forms import MealForm
+from .models import Meal
 
 # Create your views here.
 @login_required(login_url='/account/sign_in')
 def meal(request):
-    return render(request, 'meal.html', {})
+    meals = Meal.objects.filter(restaurant=request.user.restaurant).order_by("-id")
+    return render(request, 'meal.html', { "meals": meals })
 
 @login_required(login_url='/account/sign_in')
 def add_meal(request):
@@ -16,9 +19,25 @@ def add_meal(request):
             meal = meal_form.save(commit=False)
             meal.restaurant = request.user.restaurant
             meal.save()
+            return redirect(meal)
     
     meal_form = MealForm()
 
     return render(request, 'add_meal.html', {
+        "meal_form": meal_form
+    })
+
+@login_required(login_url='/account/sign_in')
+def edit_meal(request, meal_id):
+    if request.method == 'POST':
+        meal_form = MealForm(request.POST, request.FILES, instance=Meal.objects.get(id=meal_id))
+
+        if meal_form.is_valid():
+            meal_form.save()
+            return redirect(meal)
+    
+    meal_form = MealForm(instance=Meal.objects.get(id=meal_id))
+
+    return render(request, 'edit_meal.html', {
         "meal_form": meal_form
     })
