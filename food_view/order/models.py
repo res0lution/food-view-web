4c959aaa0c56e0819985ec728461448282314fdd
Res0ution <maestro@hotger.com>
2022-07-09 13:01:47 +0300

    add order model

--- food_view/order/models.py
+++ food_view/order/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
+from core.models import Customer, Driver
+from restaurant.models import Restaurant, Meal
+from django.utils import timezone
+
 # Create your models here.
+class Order(models.Model):
+    COOKING = 1
+    READY = 2
+    ONTHEWAY = 3
+    DELIVERED = 4
+
+    STATUS_CHOICES = (
+        (COOKING, "Cooking"),
+        (READY, "Ready"),
+        (ONTHEWAY, "On the way"),
+        (DELIVERED, "Delivered")
+    )
+
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.PROTECT)
+    driver = models.ForeignKey(Driver, models.SET_NULL, blank=True, null=True)
+    address = models.CharField(max_length=500)
+    total = models.IntegerField()
+    status = models.IntegerField(choices=STATUS_CHOICES)
+    created_at = models.DateTimeField(default=timezone.now)
+    picked_at = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return str(self.id)
+
+class OrderDetails(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT, related_name="order_details")
+    meal = models.ForeignKey(Meal, on_delete=models.PROTECT)
+    quantity = models.IntegerField()
+    sub_total = models.IntegerField()
+
+    def __str__(self):
+        return str(self.id)
--- food_view/order/views.py
+++ food_view/order/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+from .models import Order
+
 # Create your views here.
 @login_required(login_url='/account/sign_in')
 def order(request):
-    return render(request, 'order.html', {})
+    if request.method == 'POST':
+        order = Order.objects.get(id=request.POST["id"])
+
+        if order.status == Order.COOKING:
+            order.status = Order.READY
+            order.save()
+
+    orders = Order.objects.filter(restaurant=request.user.restaurant).order_by('-id')
+    return render(request, 'order.html', {
+        "orders": orders
+    })
 
 @login_required(login_url='/account/sign_in')
 def report(request):
